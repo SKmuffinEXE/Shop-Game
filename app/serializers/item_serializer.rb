7879a17121c290c1a9df6ab033c2c4996ac5c707
David <DEvangelista54@gmail.com>
2022-01-30 16:45:20 -0500

    slight revision to displayed json data

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :basePrice, :quality, :category_id, :recipeid
+  attributes :id, :name, :basePrice,:category_id, :recipeid
 
   belongs_to :category
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :alias, :storeName, :totalGold, :currentGold, :storageSpace, :storeSpace, :emblem, :reputation
+  attributes :id, :alias, :storeName, :totalGold, :currentGold, :storageSpace, :storeSpace, :emblem, :reputation, :item
 
-  has_many :storage
+  # has_many :storage
   # has_many :items, through: :storage
 end
